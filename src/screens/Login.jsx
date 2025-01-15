3e76b45e67156b84c767c8c8c879e6f3f1bd0e3c
rabat <itsadorable11@gmail.com>
2025-01-15 14:29:07 +0900

    로그인

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,28 +1,27 @@
 import styles from '../styles/Login.module.css';
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const nav = useNavigate();
+    const handleDivClick = () => {
+        nav("/signup");
+    };
   	return (
     		<div className={styles.login}>
       			<div className={styles.signInBg}>
-                      <div className={styles.signBtn}>
-                              <div className={styles.signInBtn} />
-                              <div className={styles.signIn}>Sign in</div>
-                      </div>
+                    <div className={styles.signInBtn} onClick={handleDivClick}>회원가입 </div>
       			</div>
-      			<div className={styles.loginBg} />
-      			<div className={styles.logo}>온길</div>
-      			<div className={styles.loginForm}>
-        				<div className={styles.idInput} />
-        				<div className={styles.pwdInput} />
-        				<div className={styles.id}>id</div>
-        				<div className={styles.password}>password</div>
-      			</div>
-      			<div className={styles.loginBtn}>
-        				<div className={styles.loginSubmitBg} />
-        				<div className={styles.logIn}>Log in</div>
-      			</div>
-      			<div className={styles.findPwdBtn}>{` 비밀번호 재설정 `}</div>
+      			<div className={styles.loginBg}>
+      			    <div className={styles.logo}>온길</div>
+      			    <div className={styles.loginForm}>
+                        <div className={styles.id}>아이디</div>
+                        <input className={styles.idInput} />
+                        <div className={styles.pwd}>비밀번호</div>
+                        <input className={styles.pwdInput} />
+                        <div className={styles.findPwdBtn}>Forgot Password</div>
+                    </div>
+                    <div className={styles.loginBtn}>로그인 </div>
+    		    </div>
     		</div>);
 };
 
