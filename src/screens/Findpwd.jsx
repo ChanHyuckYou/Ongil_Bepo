24519edefff40a265d07a6a345a0f6d88d6d73ca
yoochanhyuk <qqs4562@naver.com>
2025-01-17 15:21:26 +0900

    components 폴더명 수정 / build시 오류 문제해결

--- src/screens/Findpwd.jsx
+++ src/screens/Findpwd.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/FindPwd.module.css';
-import { useState } from 'react';
-import useNavigations from "../Components/Navigation/Navigations.jsx";
+import {useState} from 'react';
+import useNavigations from "../components/Navigation/Navigations.jsx";
 
 const FindPwd = () => {
   const [emailInput, setEmailInput] = useState(""); // 입력 필드 값
@@ -34,36 +34,39 @@ const FindPwd = () => {
     console.log("인증번호 발송"); // 인증번호 발송 로직
   };
 
-
   return (
-    <div className={styles.FindPwdContainer}>
-      <div className={styles.FindPwdForm}>
-        <div className={styles.title}>비밀번호 찾기</div>
-        {/*}<div className={styles.emailTxt}>이메일</div>*/}
+      <div className={styles.FindPwdContainer}>
+        <div className={styles.FindPwdForm}>
+          <div className={styles.title}>비밀번호 찾기</div>
+          {/*}<div className={styles.emailTxt}>이메일</div>*/}
 
-        <div className={styles.form}>
+          <div className={styles.form}>
+            <input
+                className={styles.emailInput}
+                value={emailInput} // 이메일 입력 필드 값
+                onChange={handleEmailInputChange} // 값 변경 핸들러 연결
+                placeholder="이메일 입력"
+            />
+            <div className={styles.keyBtn} onClick={save}>
+              인증번호 발송
+            </div>
+          </div>
+
+          {/*<div className={styles.numTxt}>인증번호</div>*/}
           <input
-            className={styles.emailInput}
-            value={emailInput} // 이메일 입력 필드 값
-            onChange={handleEmailInputChange} // 값 변경 핸들러 연결
-            placeholder="이메일 입력"
+              className={styles.numInput}
+              value={authNum}
+              onChange={handleAuthNumChange}
+              placeholder="인증번호 입력"
           />
-          <div className={styles.keyBtn} onClick={save}>
-            인증번호 발송
+          <div className={styles.submitBtn}
+               onClick={() => handleNavigation('Resetpwd')}>확인
+          </div>
+          <div className={styles.cancelBtn}
+               onClick={() => handleNavigation('Login')}>취소
           </div>
         </div>
-
-        {/*<div className={styles.numTxt}>인증번호</div>*/}
-        <input
-          className={styles.numInput}
-          value={authNum}
-          onChange={handleAuthNumChange}
-          placeholder="인증번호 입력"
-        />
-        <div className={styles.submitBtn} onClick={() => handleNavigation('Resetpwd')}>확인</div>
-        <div className={styles.cancelBtn} onClick={() => handleNavigation('Login')}>취소</div>
       </div>
-    </div>
   );
 };
 
