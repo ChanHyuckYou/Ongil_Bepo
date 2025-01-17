68ae176fae26602265a3512612edfb8df3c36b73
yoochanhyuk <qqs4562@naver.com>
2025-01-17 13:13:38 +0900

    Not_found page 구현, 잘못된 경로 리다이렉션

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,7 +1,6 @@
 import {useEffect} from "react";
 import {Routes, Route, useNavigationType, useLocation} from "react-router-dom";
 import Login from "../screens/Login.jsx";
-import Signup from "../screens/Signup.jsx";
 import Home from "../screens/Home.jsx";
 import AdminPage from "../screens/Adminpage.jsx";
 import BoardDetail from "../screens/BoardDetail.jsx";
@@ -14,18 +13,15 @@ import Mypage from "../screens/Mypage.jsx";
 import Findpwd from "../screens/Findpwd.jsx";
 import Resetpwd from "../screens/Resetpwd.jsx";
 import MypageInput from "../screens/MypageInput.jsx";
-import TopNavigation from "../components/nav/TopNavigation.jsx";
-import SideNavigation from "../components/nav/SideNavigation.jsx";
-import DynamicHeader from "../components/nav/DynamicHeader.jsx";
+import Layout from "../components/nav/Layout.jsx"; // Layout 컴포넌트
+import NotFound from "../screens/NotFound.jsx";
+
+/*import BoardId from "../screens/BoardId.jsx"; // 동적 경로에 대한 컴포넌트*/
 
 function Router() {
   const action = useNavigationType();
   const location = useLocation();
   const pathname = location.pathname;
-
-  // 경로에 따른 레이아웃 숨기기 설정
-  const hideLayoutForPaths = ["/", "/find-pwd", "/reset-pwd"]; // 레이아웃 숨길 경로
-  const isLayoutHidden = hideLayoutForPaths.includes(pathname);
 
   useEffect(() => {
     if (action !== "POP") {
@@ -79,12 +75,27 @@ function Router() {
         title: "Search Roads",
         description: "Search for roads information",
       },
+      "/board/:id": { // 동적 경로에 대한 메타 데이터
+        title: "Board Detail",
+        description: "View details of the board post",
+      },
     };
 
-    const {title, description} = metaData[pathname] || {
+    const defaultMeta = {
       title: "Page Not Found",
       description: "This page does not exist",
     };
+
+    const matchedPath = Object.keys(metaData).find((path) => {
+      if (path.includes(":")) {
+        // 동적 경로 처리
+        const basePath = path.split("/:")[0];
+        return pathname.startsWith(basePath);
+      }
+      return path === pathname;
+    });
+
+    const {title, description} = metaData[matchedPath] || defaultMeta;
 
     // 문서 제목 및 메타 태그 업데이트
     document.title = title;
@@ -97,33 +108,30 @@ function Router() {
   }, [pathname]);
 
   return (
-      <>
-        {/* 레이아웃 조건부 렌더링 */}
-        {!isLayoutHidden && (
-            <>
-              <TopNavigation/>
-              <SideNavigation/>
-              <DynamicHeader/>
-            </>
-        )}
-
-        {/* 라우트 정의 */}
-        <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/find-pwd" element={<Findpwd/>}/>
-          <Route path="/reset-pwd" element={<Resetpwd/>}/>
+      <Routes>
+        {/* 레이아웃이 필요한 경로들을 Layout으로 감쌈 */}
+        <Route element={<Layout/>}>
           <Route path="/home" element={<Home/>}/>
           <Route path="/admin-page" element={<AdminPage/>}/>
           <Route path="/roads-recommend" element={<RoadsRecommend/>}/>
+          <Route path="/roads-search" element={<RoadsSearch/>}/>
           <Route path="/board-main" element={<BoardMain/>}/>
           <Route path="/board-detail" element={<BoardDetail/>}/>
           <Route path="/board-create" element={<BoardCreate/>}/>
+          {/*<Route path="/board/:id" element={<BoardId/>}/>  동적 경로 */}
           <Route path="/mypage" element={<Mypage/>}/>
           <Route path="/inquire" element={<Inquire/>}/>
           <Route path="/mypage-input" element={<MypageInput/>}/>
-          <Route path="/roads-search" element={<RoadsSearch/>}/>
-        </Routes>
-      </>
+        </Route>
+
+        {/* 레이아웃이 필요 없는 경로들 */}
+        <Route path="/" element={<Login/>}/>
+        <Route path="/find-pwd" element={<Findpwd/>}/>
+        <Route path="/reset-pwd" element={<Resetpwd/>}/>
+
+        {/* 잘못된 경로 처리 */}
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
   );
 }
 
