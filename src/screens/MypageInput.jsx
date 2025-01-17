74d322ff16b544744914dbdcf35e22e68f50097b
rabbitbaka <95219232+rabbitbaka@users.noreply.github.com>
2025-01-17 13:52:23 +0900

    MypageInput 페이지

--- src/screens/MypageInput.jsx
+++ src/screens/MypageInput.jsx
@@ -3,20 +3,15 @@ import styles from '../styles/MypageInput.module.css';
 const MypageInput = () => {
   return (
       <div className={styles.mypageinput}>
-
-        <div className={styles.head}>
-          <b className={styles.headTxt}>내 정보 수정</b>
-          <div className={styles.headLine}/>
-        </div>
         <div className={styles.mypageForm}>
-          <div className={styles.pwdForm}/>
-          <div className={styles.inputFuild}/>
-          <div className={styles.okBtn}>
-            <div className={styles.okBg}/>
-            <b className={styles.okTxt}>확인</b>
+          <div className={styles.pwdForm}>
+              <img className={styles.mypageImgIcon} alt="" src="/images/Mypage_img.png"/>
+              <div className={styles.inputform}>
+                  <div className={styles.pwdcheckTxt}>기존 비밀번호 확인</div>
+                  <input className={styles.inputFuild}/>
+                  <div className={styles.okBtn}>확인</div>
+              </div>
           </div>
-          <b className={styles.pwdcheckTxt}>기존 비밀번호 확인</b>
-          <img className={styles.mypageImgIcon} alt="" src="Mypage_img.png"/>
         </div>
       </div>);
 };
