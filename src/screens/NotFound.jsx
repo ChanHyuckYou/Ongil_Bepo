e7220ac09bfba4cefdcd2771ef12f4cde9bf4b66
rabbitbaka <95219232+rabbitbaka@users.noreply.github.com>
2025-01-30 17:07:53 +0900

    NotFound.module.css로 변경

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -1,6 +1,7 @@
 // NotFound.jsx
 import 'react';
-import '../styles/NotFound.css';
+import styles from '../styles/NotFound.module.css';
+
 
 const NotFound = () => {
   const handleGoHome = () => {
@@ -12,22 +13,22 @@ const NotFound = () => {
   };
 
   return (
-      <div className="not-found-container">
-        <div className="image">
+      <div className={styles.container}>
+        <div className={styles.image}>
           <img
               src="/images/notfound.png"
               srcSet="/images/notfound-small.png 480w, /images/page-not-found.png"
               alt="Page Not Found Illustration"
           />
         </div>
-        <h1>404</h1>
-        <p>페이지를 찾을 수 없습니다. 잘못된 URL이거나 페이지가 삭제되었을 수 있습니다.</p>
+        <h1 className={styles.h1}>404</h1>
+        <p className={styles.p}>페이지를 찾을 수 없습니다. 잘못된 URL이거나 페이지가 삭제되었을 수 있습니다.</p>
 
-        <div className="btn-container">
-          <button onClick={handleGoHome} className="btn" aria-label="홈으로 가기">
+        <div className={styles.btn_container}>
+          <button onClick={handleGoHome} className={styles.btn} aria-label="홈으로 가기">
             홈으로 가기
           </button>
-          <button onClick={handleGoBack} className="btn"
+          <button onClick={handleGoBack} className={styles.btn}
                   aria-label="이전 페이지로 돌아가기">
             이전 페이지로 돌아가기
           </button>
